ca255742bd6cb92a6694027024a580840d4fea47
Elizabeth Burkholder <e-burkholder@onu.edu>
2023-10-19 10:41:21 -0400

    completed p8-2.

--- p8-2/Source.cpp
+++ p8-2/Source.cpp
@@ -5,24 +5,25 @@ using namespace std;
 int countWords(string str);
 
 int main() {
-	cout<<countWords("Hello it is I.")<<endl;
-	cout<<countWords("one")<<endl;
-	cout<<countWords("one 2 three    4 five");
+	string input;
+	while (true) {
+		cout << "Enter a string or Q to quit: ";
+		getline(cin, input);
+		if (input == "Q")
+			break;
+		cout << "Words: " << countWords(input)<<endl;
+	}
 
 	return 0;
 }
 
 int countWords(string str) {
 	int characters = str.length();
-	int space = 0;
+	int space = 1;
 	for (int i = 0; i < characters; i++) {
 		str.substr(i, 1);
-		if (str.substr(i, 1) == " ") {
+		if (str.substr(i, 1) == " " && str.substr(i - 1, 1) != " ") {
 			space++;
-			while (true) {
-				i++;
-				break;
-			}
 		}
 	}
 	return space;
