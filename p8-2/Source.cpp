11e758e30f77a0206a8281ec035399a8be5c8d60
Elizabeth Burkholder <e-burkholder@onu.edu>
2023-10-19 10:07:41 -0400

    Completed countWords function.

--- p8-2/Source.cpp
+++ p8-2/Source.cpp
@@ -1,1 +1,30 @@
-//p8-2
+#include <iostream>
+#include <string>
+using namespace std;
+
+int countWords(string str);
+
+int main() {
+	cout<<countWords("Hello it is I.")<<endl;
+	cout<<countWords("one")<<endl;
+	cout<<countWords("one 2 three    4 five");
+
+	return 0;
+}
+
+int countWords(string str) {
+	int characters = str.length();
+	int space = 0;
+	for (int i = 0; i < characters; i++) {
+		str.substr(i, 1);
+		if (str.substr(i, 1) == " ") {
+			space++;
+			while (true) {
+				i++;
+				break;
+			}
+		}
+	}
+	return space;
+
+}
